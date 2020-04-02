2c7b84f1a7cd2f765cb7d59d8383592890a904b3
yhf <1042157616@qq.com>
2020-04-03 00:03:22 +0800

    init eureka

---
+++ src/test/java/edu/qingtai/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.qingtai.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
